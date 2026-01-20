cdb9f7208eae2ac020c0f6bae3446a76da54a325
vhbiten <victorhugo_biten@hotmail.com>
2026-01-20 13:05:58 -0400

    refactor display and feat reduce quantity item

--- src/main.js
+++ src/main.js
@@ -10,13 +10,24 @@ console.log("Bem vindo ao seu carrinho de compras!\n")
 const item1 = await createItem("monitor", 1200, 2)
 const item2 = await createItem("teclado", 119.90, 1)
 
+//adicionando itens ao carrinho
 await cartService.addItem(cart, item1)
 await cartService.addItem(wishList, item2)
 
-console.log("Total do carrinho: ")
-await cartService.calculateTotal(cart)
+//deletando itens do carrinho
+// await cartService.deleteItem(cart, item1.name)
 
-console.log("Total da Lista de desejos: ")
+//reduzindo a  quantidade de um item do carrinho
+await cartService.reduceQuantity(cart, 0)
+
+
+//exibindo os itens do carrinho
+await cartService.displayCart(cart, "cart")
+await cartService.displayCart(wishList, "wishlist")
+
+
+//calculando os totais do carrinho
+await cartService.calculateTotal(cart)
 await cartService.calculateTotal(wishList)
 
 
--- src/services/cart.js
+++ src/services/cart.js
@@ -3,22 +3,49 @@ async function addItem(userCart, item) {
 }
 
 async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name)
 
+    if (index !== -1){
+        userCart.splice(index, 1)
+    }
 }
 
-async function removeItem(userCart, index){
-
+async function reduceQuantity(userCart, index){
+    const deleteIndex = index -1
+    if (index => 0 && userCart.length){
+        userCart.splice(deleteIndex, 1)
+    }
 }
 
-async function calculateTotal(userCart) {
+async function calculateTotal(userCart, type = "cart") {
     const result = userCart.reduce((total, item)=> total + item.subtotal(), 0)
-    console.log(result)
+
+    if (type === "cart"){
+        console.log(`Total do carrinho: ${result}`)
+    }else {
+        console.log(`Total da lista de desejos: ${result}`)
+    }
+    
 }
+
+async function displayCart(userCart, type = "cart") {
+    if (type === "cart"){
+        console.log("### Itens do carrinho: ###\n")
+    }else {
+        console.log("### Itens da Lista de desejos: ###\n")
+    }
+
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal = ${item.subtotal()} \n`)
+    })
+}
+
 
 
 export {
     addItem,
     deleteItem,
-    removeItem,
-    calculateTotal
+    reduceQuantity,
+    calculateTotal,
+    displayCart
 }
